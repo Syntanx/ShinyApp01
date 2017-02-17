ee12313020a551f15687c317a83ac89c27870a87
Alexander Giddey <gddale001@myuct.ac.za>
2017-02-17 12:04:21 +0200

    Multiple Province select and addition of table with it's checkBox input

--- first_app/app.R
+++ first_app/app.R
@@ -11,6 +11,7 @@ library(shiny)
 library(southafricastats)
 library(dplyr)
 library(ggplot2)
+library(DT)
 
 mortality_noAllCause <- mortality_zaf %>%
   filter(indicator != "All causes")
@@ -27,13 +28,18 @@ ui <- fluidPage(
          selectInput(inputId = "province",
                      label = "Province",
                      choices = unique(mortality_zaf$province),
-                     selected = "Gauteng"
-                     )
+                     selected = "Gauteng",
+                     multiple = T
+                     ),
+         checkboxInput(inputId = "showTable",
+                       label = "Display Table",
+                       value = F)
       ),
       
       # Show a plot
       mainPanel(
-         plotOutput(outputId = "LinePlot")
+         plotOutput(outputId = "LinePlot"),
+         dataTableOutput(outputId = "mortalityTable")
       )
    )
 )
@@ -43,10 +49,19 @@ server <- function(input, output) {
    
    output$LinePlot <- renderPlot({
      mortality_noAllCause %>% 
-       filter(province==input$province) %>% 
+       filter(province %in% input$province) %>% 
         ggplot(aes(x = year, y = deaths, color = indicator)) + 
           geom_line(alpha = 0.8, size = 1.5) + 
+          facet_wrap(~province) +
           theme_minimal(base_size = 18)
+   })
+   
+   output$mortalityTable <- renderDataTable({
+     if (input$showTable) {
+       datatable(mortality_noAllCause %>% 
+         filter(province %in% input$province)
+       ) 
+     }
    })
 }
 
